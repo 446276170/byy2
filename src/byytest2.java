57bdbc7eb13c61216daf8eb38b306c3a96f9831b
byy <446276170@qq.com>
2017-03-25 04:31:20 +0800

    byytest2 2017-3-25 04:31:11

---
+++ src/byytest2.java
@@ -0,0 +1,252 @@
+import java.*;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by Administrator on 2017/3/22/022.
+ */
+public class byytest2 {
+    ArrayList<String> list1 = new ArrayList<String>();
+    Scanner s = new Scanner(System.in);
+    DecimalFormat df = new DecimalFormat("######0.00"); //取小数后两位
+    double ture=0,all=0;   //题目正确率计数
+
+/*    String [] getrandom() {
+        String [] number = new String[2];
+        switch ((int) (Math.random() * 2)) {
+            case 0:
+                int num;
+                num = (int) (Math.random() * 99) + 1;
+                number[0] = String.valueOf(num);
+                number[1] = "ZS";
+                break;
+            default:
+                int n1 = (int) (Math.random() * 99) + 1;
+                int n2 = (int) (Math.random() * (99 - n1)) + n1 + 1;
+                number[0] = String.valueOf(n1) + "/" + String.valueOf(n2);
+                number[1] = "FS";
+                break;
+        }
+        return number;
+    } */
+
+    void getrandomnum() {
+        list1.clear();  //完成一题后清除数组
+        int n1, n2, n3, n4;
+        int temp = (int) (Math.random() * 3);   //运算种类判断
+
+        if (temp == 0) {   //整数运算
+            n1 = (int) (Math.random() * 99) + 1;
+            n2 = (int) (Math.random() * 99) + 1;
+            list1.add(String.valueOf(n1));
+            list1.add("ZS");
+            list1.add(String.valueOf(n2));
+            list1.add("ZS");
+        }
+        else if (temp == 1) {  //整数分数运算
+            n1 = (int) (Math.random() * 99) + 1;
+            n2 = (int) (Math.random() * 99) + 1;
+            n3 = (int) (Math.random() * (99 - n2)) + n2 + 1;
+            //n3 = (int) (Math.random() * 99) + 1;
+            list1.add(String.valueOf(n1));
+            list1.add("ZS");
+            //if(n2>n3)
+            //    list1.add(String.valueOf(n3 + "/" + n2));
+            //else
+            list1.add(String.valueOf(n2 + "/" + n3));
+            list1.add("FS");
+        }
+        else if (temp == 2) {  //分数整数运算
+            n1 = (int) (Math.random() * 99) + 1;
+            n2 = (int) (Math.random() * 99) + 1;
+            n3 = (int) (Math.random() * (99 - n2)) + n2 + 1;
+            //n3 = (int) (Math.random() * 99) + 1;
+            list1.add(String.valueOf(n2 + "/" + n3));
+            list1.add("FS");
+            //if(n2>n3)
+            //    list1.add(String.valueOf(n3 + "/" + n2));
+            //else
+            list1.add(String.valueOf(n1));
+            list1.add("ZS");
+        }
+        else {   //分数运算
+            n1 = (int) (Math.random() * 99) + 1;
+            n2 = (int) (Math.random() * (99 - n1)) + n1 + 1;
+            //n2 = (int) (Math.random() * 99) + 1;
+            n3 = (int) (Math.random() * 99) + 1;
+            n4 = (int) (Math.random() * (99 - n3)) + n3 + 1;
+            //n4 = (int) (Math.random() * 99) + 1;
+            //if (n1>n2)
+            //    list1.add(String.valueOf(n2 + "/" + n1));
+            //else
+            list1.add(String.valueOf(n1 + "/" + n2));
+            list1.add("FS");
+            //if (n3>n4)
+            //    list1.add(String.valueOf(n4 + "/" + n3));
+            //else
+            list1.add(String.valueOf(n3 + "/" + n4));
+            list1.add("FS");
+        }
+    }
+    void text() {   //题目输出函数
+        getrandomnum(); //随机数产生
+        int a=(int)all+1;   //题目计数，便于观看
+        String []num1 = new String[2];
+        String []num2 = new String[2];
+        num1[0] = list1.get(0);    //取出数组中的整数或分数
+        num2[0] = list1.get(2);
+        int flag = (int) (Math.random() * 4);   //运算符判断
+        if (flag == 0)
+            System.out.println(a + ". " + num1[0] + " + " + num2[0] + " = ?");
+        else if (flag == 1)
+            System.out.println(a + ". " + num1[0] + " - " + num2[0] + " = ?");
+        else if (flag == 2)
+            System.out.println(a + ". " + num1[0] + " * " + num2[0] + " = ?");
+        else
+            System.out.println(a + ". " + num1[0] + " / " + num2[0] + " = ?");
+
+        Calculate(num1,num2,flag);
+    }
+    void Calculate(String []num1,String []num2,int flag){   //计算函数
+        String result = "";
+        num1[1] = list1.get(1);    //取出数组中的整数或分数
+        num2[1] = list1.get(3);
+
+        if(num1[1]=="ZS" && num2[1]=="ZS"){
+            //num1 = num1[0].split("/");
+            //num2 = num2[0].split("/");
+            int num11 = Integer.parseInt(num1[0]);  //整数1
+            int num21 = Integer.parseInt(num2[0]);  //整数2
+            if(flag == 0){
+                result = String.valueOf(num11 + num21);
+            }
+            else if (flag == 1){
+                result = String.valueOf(num11 - num21);
+            }
+            else if (flag == 2){
+                result = String.valueOf(num11 * num21);
+            }
+            else {
+                result = String.valueOf(num11 + "/" + num21);
+            }
+        }
+        else if (num1[1]=="ZS" && num2[1]=="FS"){
+            //num1 = num1[0].split("/");
+            num2 = num2[0].split("/");
+            int num11 = Integer.parseInt(num1[0]);  //整数
+            int num21 = Integer.parseInt(num2[0]);  //分数分子
+            int num22 = Integer.parseInt(num2[1]);  //分数分母
+            if(flag == 0){
+                result = String.valueOf((num11 * num22) + num21 + "/" + num22);
+            }
+            else if (flag == 1){
+                result = String.valueOf((num11 * num22) - num21 + "/" + num22);
+            }
+            else if (flag == 2){
+                result = String.valueOf((num11 * num21) + "/" + num22);
+            }
+            else {
+                result = String.valueOf((num11 * num22) + "/" + num21);
+            }
+        }
+        else if (num1[1]=="FS" && num2[1]=="ZS"){
+            num1 = num1[0].split("/");
+            //num2 = num2[0].split("/");
+            int num11 = Integer.parseInt(num1[0]);  //分数分子
+            int num12 = Integer.parseInt(num1[1]);  //分数分母
+            int num21 = Integer.parseInt(num2[0]);  //整数
+            if(flag == 0){
+                result = String.valueOf(num11 + (num12 * num21) + "/" + num12);
+            }
+            else if (flag == 1){
+                result = String.valueOf(num11 - (num12 * num21) + "/" + num12);
+            }
+            else if (flag == 2){
+                result = String.valueOf((num11 * num21) + "/" + num12);
+            }
+            else {
+                result = String.valueOf(num11 + "/" + (num12*num21));
+            }
+        }
+        else {
+            num1 = num1[0].split("/");
+            num2 = num2[0].split("/");
+            int num11 = Integer.parseInt(num1[0]);  //分数1分子
+            int num12 = Integer.parseInt(num1[1]);  //分数1分母
+            int num21 = Integer.parseInt(num2[0]);  //分数2分子
+            int num22 = Integer.parseInt(num2[1]);  //分数2分母
+            if(flag == 0){
+                result = String.valueOf(((num11 * num22) + (num12 * num21)) + "/" + (num12 * num22));
+            }
+            else if (flag == 1){
+                result = String.valueOf(((num11 * num22) - (num12 * num21)) + "/" + (num12 * num22));
+            }
+            else if (flag == 2){
+                result = String.valueOf((num11 * num21) + "/" + (num12 * num22));
+            }
+            else {
+                result = String.valueOf((num11 * num22) + "/" + (num12 * num21));
+            }
+        }
+        if (result.indexOf("/")!= -1) {
+            result = huajian(result);
+        }
+        System.out.println("请输入最简答案:");
+        String line = s.nextLine();
+        if (line.compareTo(result) == 0) {
+            System.out.println("正确！");
+            ture++;
+        }
+        else{
+            System.out.println("错误！答案为" + result + "。");
+        }
+        all++;
+        System.out.println("正确率为" + df.format(ture/all*100) + "%。");
+    }
+    String huajian(String result){
+        String s[] = result.split("/"); //获取未化简的答案
+        String newresult = "";  //化简的新答案
+        int n1 = Math.abs(Integer.parseInt(s[0]));    //分子
+        int n2 = Math.abs(Integer.parseInt(s[1]));    //分母
+        int maxcommon = 1;  //最大公约数
+        if (n1>n2){ //如果分子比分母大
+            while(n1 % n2 != 0){
+                maxcommon = n1 % n2;
+                n1 = n2;
+                n2 = maxcommon;
+            }
+            newresult = String.valueOf((Integer.parseInt(s[0])/maxcommon) + "/" + (Integer.parseInt(s[1])/maxcommon));
+        }
+        else{   //如果分子比分母小，先交换再计算
+            int t = n1;
+            n1 = n2;
+            n2 = t;
+            while(n1 % n2 != 0){
+                maxcommon = n1 % n2;
+                n1 = n2;
+                n2 = maxcommon;
+            }
+            newresult = String.valueOf((Integer.parseInt(s[0])/maxcommon) + "/" + (Integer.parseInt(s[1])/maxcommon));
+        }
+        return newresult;   //传回新值
+    }
+    public static void main(String[] args)
+    {
+        byytest2 b = new byytest2();
+        System.out.println("来做题吧?(输入 exit 退出 Enter键出题)");
+        //String scan = b.s.nextLine();
+        while(true) {
+            if (b.s.nextLine().equals("exit")){
+                break;
+            }
+            else {
+                b.text();
+            }
+        }
+        //String str = "abcdefga";
+        //System.out.println(str.indexOf("q"));
+    }
+}
+//基本功能实现 待完善 2017年3月25日04:22:05
